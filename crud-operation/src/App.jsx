50e4f3f730688a3bd619aec72b1d40f334424882
Noman <noman5456318@gmail.com>
2023-09-30 00:15:19 +0500

    first page completed

--- crud-operation/src/App.jsx
+++ crud-operation/src/App.jsx
@@ -1,13 +1,27 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import {FiSearch} from 'react-icons/fi'
+import {AiFillPlusCircle} from 'react-icons/ai'
 
 function App() {
  
 
   return (
-   <div>
+   <div className='mx-auto max-w-[370px] px-4'>
     <Navbar/>
+
+ 
+    <div className="flex gap-2">
+    <div className="flex flex-grow items-center relative  ">
+      <FiSearch className='ml-1 absolute text-3xl text-white p-1'/>
+      <input type="text"
+      className='h-10 flex-grow rounded-md border border-white
+      bg-transparent outline-none pl-10 text-white text-[19px]'
+      name="" id="" />
+    </div>
+    <AiFillPlusCircle className='text-5xl text-white cursor-pointer '/>
+    </div>
    </div>
   )
 }
--- crud-operation/src/components/Navbar.jsx
+++ crud-operation/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     
-    <div className=''>
+    <div className='h-[60px] my-4 bg-white flex justify-center items-center rounded-lg gap-2 font-medium text-xl'>
+        <img src="/logos_firebase.png"  />
+        <h1>React-CRUD-App</h1>
 
     </div>
   )

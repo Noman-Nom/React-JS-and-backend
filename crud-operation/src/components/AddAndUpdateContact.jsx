ba2a967025c9e535aa0fe277657ee4b16883a354
Noman <noman5456318@gmail.com>
2023-09-30 21:02:14 +0500

    data send to firebase successfully

--- crud-operation/src/components/AddAndUpdateContact.jsx
+++ crud-operation/src/components/AddAndUpdateContact.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import Model from './Model'
 import { Field, Form, Formik } from 'formik'
+import {addDoc, collection} from 'firebase/firestore'
+import {db} from '../config/firebase'
 
 const AddAndUpdateContact = ({ isOpen, onClose }) => {
+
+    const addContact = async (contact)=>{
+
+        try {
+            const contactRef = collection(db,"contacts")
+            await addDoc(contactRef,contact)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div>
             <Model
@@ -16,6 +29,7 @@ const AddAndUpdateContact = ({ isOpen, onClose }) => {
                         }}
                         onSubmit={(values)=>{
                             console.log(values)
+                            addContact(values)
                         }}
                 >
                     <Form>
@@ -25,7 +39,7 @@ const AddAndUpdateContact = ({ isOpen, onClose }) => {
                         </div>
                         <div className="flex flex-col gap-1">
                         <label htmlFor="email">Email</label>
-                        <Field className="h-10 border bg-transparent" type="email" name = "email"/>
+                        <Field className="h-10 border bg-transparent"  name = "email"/>
                         </div>
                         <button className=' border bg-orange py-2 px-3 my-2 text-[15px'>Add Contact</button>
                     </Form>

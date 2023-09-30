dfd8ae5beb32a637d3ef18120ed79b0381dcc7c8
Noman <noman5456318@gmail.com>
2023-09-30 12:31:30 +0500

    working started

--- crud-operation/src/App.jsx
+++ crud-operation/src/App.jsx
@@ -14,6 +14,8 @@ function App() {
 
   const [contacts, setContacts] = useState([])
 
+  const [open, setOpen] = useState(false);
+
 
   useEffect(() => {
 
---
+++ crud-operation/src/components/Model.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Model = ({onClose, isOpen,children}) => {
+  return (
+    <div>Model</div>
+  )
+}
+
+export default Model

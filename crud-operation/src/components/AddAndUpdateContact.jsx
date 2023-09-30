d816658cff961d5b75c31adb265c0ba9196ad179
Noman <noman5456318@gmail.com>
2023-09-30 20:45:56 +0500

    form  working completed

--- crud-operation/src/components/AddAndUpdateContact.jsx
+++ crud-operation/src/components/AddAndUpdateContact.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import Model from './Model'
+import { Field, Form, Formik } from 'formik'
 
-const AddAndUpdateContact = ({isOpen,onClose}) => {
-  return (
-    <div>
+const AddAndUpdateContact = ({ isOpen, onClose }) => {
+    return (
+        <div>
             <Model
-        isOpen={isOpen}
-        onClose={onClose}
-    >Hi</Model>
-    </div>
-  )
+                isOpen={isOpen}
+                onClose={onClose}>
+
+                <Formik 
+                        initialValues={{
+                            name: "",
+                            email: "",
+                        }}
+                        onSubmit={(values)=>{
+                            console.log(values)
+                        }}
+                >
+                    <Form>
+                        <div className="flex flex-col gap-1">
+                        <label htmlFor="name">Name</label>
+                        <Field  className="h-10 border bg-transparent" name = "name"/>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                        <label htmlFor="email">Email</label>
+                        <Field className="h-10 border bg-transparent" type="email" name = "email"/>
+                        </div>
+                        <button className=' border bg-orange py-2 px-3 my-2 text-[15px'>Add Contact</button>
+                    </Form>
+                </Formik>
+
+            </Model>
+        </div>
+    )
 }
 
 export default AddAndUpdateContact

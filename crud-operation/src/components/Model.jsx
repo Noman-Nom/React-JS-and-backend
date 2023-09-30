59cbdcdfb7136258a9de19b53f751433e08f0273
Noman <noman5456318@gmail.com>
2023-09-30 13:30:00 +0500

    model ui completed

--- crud-operation/src/components/Model.jsx
+++ crud-operation/src/components/Model.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
-const Model = ({onClose, isOpen,children}) => {
-  return (
-        <div>{isOpen && <div>Model</div>}</div>
-  )
+
+const Model = ({ onClose, isOpen, children }) => {
+    return (
+        <>
+            {
+                isOpen && (
+
+                    <>
+                    <div className='bg-white m-auto min-h-[200px] max-w-[60%] p-4 relative z-50 transition-all -top-20 ' >
+                        <div className='flex justify-end'>
+                            <AiOutlineCloseCircle onClick={onClose}  className='text-3xl cursor-pointer '/>
+                        </div>
+                    </div>
+                    <div  onClick={onClose} className='absolute top-0 h-screen w-screen backdrop-blur z-40'/>
+                    </>
+                )
+
+            }
+
+
+        </>
+    )
 }
 
 export default Model

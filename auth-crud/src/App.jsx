cb9c113f7f327c277c62c2013035383d1d7d5433
Noman <noman5456318@gmail.com>
2023-10-01 21:43:30 +0500

    Router and login navigation completed

--- auth-crud/src/App.jsx
+++ auth-crud/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import Login from './components/Login/Login'
-
+import Layout from './Layout.jsx'
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
 
 function App() {
   
 
   return (
-    <>
-      <Login/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout/>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App

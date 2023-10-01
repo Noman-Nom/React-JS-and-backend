cfcb1c166a5685bcc61c306c6e6bfd6f90001a44
Noman <noman5456318@gmail.com>
2023-10-01 15:02:25 +0500

    auth started

--- auth-crud/src/components/Login/Login.jsx
+++ auth-crud/src/components/Login/Login.jsx
@@ -5,30 +5,31 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../Config/Firebase';
 
 const Login = () => {
- 
-    const [error, setError] = useState(false)
 
-    const handleLogin = (e)=>{
-      e.preventDefault()
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-      createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+  const handleLogin = (e) => {
+    e.preventDefault()
 
-    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user)
+      })
+      .catch((error) => {
+    setError(true)
+        // ..
+      });
+
+  }
   return (
     <div className='login'>
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder='email' />
-        <input type="password" placeholder='password' />
+        <input type="email" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type='submit'>Login</button>
         {error && <span>wrong email or password</span>}
       </form>

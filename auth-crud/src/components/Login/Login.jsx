585c4ff7092691b8079cbb606d58316ca88d0219
Noman <noman5456318@gmail.com>
2023-10-01 20:07:57 +0500

    Router working started

--- auth-crud/src/components/Login/Login.jsx
+++ auth-crud/src/components/Login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   const handleLogin = (e) => {
     e.preventDefault()

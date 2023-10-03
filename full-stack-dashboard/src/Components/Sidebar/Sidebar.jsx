72f3cdb089ca8d992e6ac400784a7e8fca2d6b17
Noman <noman5456318@gmail.com>
2023-10-04 00:08:38 +0500

    scss started

--- full-stack-dashboard/src/Components/Sidebar/Sidebar.jsx
+++ full-stack-dashboard/src/Components/Sidebar/Sidebar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Sidebar.scss'
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+    return (
+        <div className='sidebar'>
+
+            <div className='top'>
+                <span className="logo">
+                    <img src="/logo1.png" alt="" />
+                </span>
+
+            </div>
+            <div className='center'>
+                <ul>
+                    <li><span>Dashboard</span></li>
+                    <li><span>Dashboard</span></li>
+                    <li><span>Dashboard</span></li>
+                    <li><span>Dashboard</span></li>
+                </ul>
+            </div>
+            <div className='bottom'>color option</div>
+        </div>
+    )
 }
 
 export default Sidebar
--- full-stack-dashboard/src/Pages/Home/Home.jsx
+++ full-stack-dashboard/src/Pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Home.scss'
+import Sidebar from '../../Components/Sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Sidebar/>
+      <div className="homeContainer">
+        Container
+      </div>
+    </div>
   )
 }
 

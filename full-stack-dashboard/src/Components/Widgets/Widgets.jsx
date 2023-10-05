f1c3ae630c556d2df06d27b457d2b3089d8d0eaf
Noman <noman5456318@gmail.com>
2023-10-06 01:34:30 +0500

    widgets working started

--- full-stack-dashboard/src/Components/Widgets/Widgets.jsx
+++ full-stack-dashboard/src/Components/Widgets/Widgets.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './Widgets.scss'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+
 
 const Widgets = () => {
   return (
-    <div>
-        <div className="left">Left</div>
-        <div className="right">Right</div>
+    <div className='widget'>
+        <div className="left">
+                <span className="title">
+                    STUDENTS
+                </span>
+                <span className="counter">
+                    57971
+                </span>
+                <span className="Link">
+                    See All Students
+                </span>
+        </div>
+        <div className="right">
+            <div className="percentage">
+         <ArrowUpwardIcon/>
+                    20%
+            </div>
+                <AccountBoxIcon/>
+        </div>
     </div>
   )
 }
--- full-stack-dashboard/src/Pages/Home/Home.jsx
+++ full-stack-dashboard/src/Pages/Home/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
             <Widgets/>
             <Widgets/>
             <Widgets/>
+            
           </div>
       </div>
     </div>

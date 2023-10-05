79b1d9d7b5516e61233423e86f2345aafa7b7198
Noman <noman5456318@gmail.com>
2023-10-05 23:56:25 +0500

    all icons imported

--- full-stack-dashboard/src/Components/Navbar/Navbar.jsx
+++ full-stack-dashboard/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+import LanguageIcon from '@mui/icons-material/Language';
 import './Sidebar.scss'
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 const Navbar = () => {
   return (
@@ -7,6 +14,28 @@ const Navbar = () => {
       <div className="wrapper">
         <div className="search">
           <input type="text" placeholder='Search...' />
+          <SavedSearchIcon/>
+          
+        </div>
+        <div className="items">
+          <div className="item">
+                <LanguageIcon />
+          </div>
+          <div className="item">
+                <Brightness4Icon/>
+          </div>
+          <div className="item">
+                <FullscreenIcon/>
+          </div>
+          <div className="item">
+                <CircleNotificationsIcon/>
+          </div>
+          <div className="item">
+               <ChatBubbleIcon/>
+          </div>
+          <div className="item">
+               <FormatListBulletedIcon/>
+          </div>
         </div>
       </div>
     </div>

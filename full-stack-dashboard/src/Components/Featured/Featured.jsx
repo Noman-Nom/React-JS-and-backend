29b86f9a1aae7c8cce97117ec77ddaf59b2980d8
Noman <noman5456318@gmail.com>
2023-10-06 21:57:25 +0500

    chart section

--- full-stack-dashboard/src/Components/Featured/Featured.jsx
+++ full-stack-dashboard/src/Components/Featured/Featured.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Featured.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Featured = () => {
   return (
-    <div className='Featured'>Featured</div>
+    <div className='Featured'>
+      
+      <div className="top">
+          <h1 className="title">University Survey</h1>
+          <MoreVertIcon fontSize='small'/>
+      </div>
+      <div className="bottom">
+
+      </div>
+    </div>
   )
 }
 

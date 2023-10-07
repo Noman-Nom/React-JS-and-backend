43377834de7889869116b7eddda57ba6e0eccdbd
Noman <noman5456318@gmail.com>
2023-10-08 03:09:08 +0500

    dummy data displayed

--- full-stack-dashboard/src/DataTableSource.jsx
+++ full-stack-dashboard/src/DataTableSource.jsx
@@ -6,7 +6,7 @@ export const userColums = [
         field: "user",
         headerName: "User",
         width: 290,
-        renderCell: (params)=>{
+        renderCell: (params) => {
             return (
                 <div className="cellWithImg">
                     <img className="cellImg" src={params.row.img} alt={params.row.name} />
@@ -16,69 +16,80 @@ export const userColums = [
             )
         }
     },
-    { field: 'email', headerName: 'Email', width:230  },
-    { field: 'UserType', headerName: 'User Type', width:130  },
-    { field: 'status', headerName: 'Status', width:100  },
+    { field: 'email', headerName: 'Email', width: 230 },
+    { field: 'UserType', headerName: 'User Type', width: 130 },
+    {
+        field: 'status', headerName: 'Status', width: 160,
+        renderCell: (params) => {
+            return (
+                <div className={`cellWithStatus ${params.row.status}`}>
+                    {
+                        params.row.status
+                    }
+                </div>
+            )
+        }
+    },
 ]
 export const userData = [
     {
         id: 1,
-        name:"Ghazanfarullah Khan",
-        img :"/sir.jpeg",
-        email:"ghazanfarkhan@uit.edu",
-        UserType:"Dr Prof",
-        status:"Active"
+        name: "Ghazanfarullah Khan",
+        img: "/sir.jpeg",
+        email: "ghazanfarkhan@uit.edu",
+        UserType: "Dr Prof",
+        status: "Active"
 
     },
-    
+
     {
         id: 2,
-        name:"Muhammad Noman",
-        img :"/noman.jpg",
-        email:"noman@uit.edu",
-        UserType:"Student",
-        status:"In Active"
+        name: "Muhammad Noman",
+        img: "/noman.jpg",
+        email: "noman@uit.edu",
+        UserType: "Student",
+        status: "InActive"
 
     },
-    
+
     {
         id: 3,
-        name:"Rabia Bilal",
-        img :"/sample.jpg",
-        email:"rbilal@uit.edu",
-        UserType:"Egr, Prof",
-        status:"Active"
+        name: "Rabia Bilal",
+        img: "/sample.jpg",
+        email: "rbilal@uit.edu",
+        UserType: "Egr, Prof",
+        status: "Active"
 
     },
-    
+
     {
         id: 4,
-        name:"Raza Muhammad ",
-        img :"/noman.jpg",
-        email:"rmuhammad@uit.edu",
-        UserType:"Dr Prof",
-        status:"Active"
+        name: "Raza Muhammad ",
+        img: "/noman.jpg",
+        email: "rmuhammad@uit.edu",
+        UserType: "Dr Prof",
+        status: "Active"
 
     },
-    
+
     {
         id: 5,
-        name:"Sheraz Ahmed",
-        img :"/noman.jpg",
-        email:"noman@uit.edu",
-        UserType:"Student",
-        status:"In Active"
+        name: "Sheraz Ahmed",
+        img: "/noman.jpg",
+        email: "noman@uit.edu",
+        UserType: "Student",
+        status: "InActive"
 
     },
-    
+
     {
         id: 6,
-        name:"Ayesha Usman",
-        img :"/sample.jpg",
-        email:"rbilal@uit.edu",
-        UserType:"Egr, Prof",
-        status:"Active"
+        name: "Ayesha Usman",
+        img: "/sample.jpg",
+        email: "rbilal@uit.edu",
+        UserType: "Egr, Prof",
+        status: "Active"
 
     },
-    
+
 ]

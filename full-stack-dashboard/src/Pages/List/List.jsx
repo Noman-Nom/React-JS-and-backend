65b61547dc53cf6fe2222690a0f7649cc6a10aae
Noman <noman5456318@gmail.com>
2023-10-08 01:00:48 +0500

    list working stated

--- full-stack-dashboard/src/Pages/List/List.jsx
+++ full-stack-dashboard/src/Pages/List/List.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Navbar from '../../Components/Navbar/Navbar'
+import './List.scss'
 
 const List = () => {
   return (
     <div className='list'>
       <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        data
+      </div>
       
     </div>
   )

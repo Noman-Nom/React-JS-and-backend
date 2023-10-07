88e59ffc653bfd370a258d09ae740907cadf31d0
Noman <noman5456318@gmail.com>
2023-10-08 01:46:53 +0500

    dummy data created

---
+++ full-stack-dashboard/src/DataTableSource.js
@@ -0,0 +1,62 @@
+export const userData = [
+    {
+        id: 1,
+        name:"Ghazanfarullah Khan",
+        img :"/sir.jpeg",
+        email:"ghazanfarkhan@uit.edu",
+        UserType:"Dr Prof",
+        status:"Active"
+
+    },
+    
+    {
+        id: 2,
+        name:"Muhammad Noman",
+        img :"/noman.jpg",
+        email:"noman@uit.edu",
+        UserType:"Student",
+        status:"In Active"
+
+    },
+    
+    {
+        id: 3,
+        name:"Rabia Bilal",
+        img :"/sample.jpg",
+        email:"rbilal@uit.edu",
+        UserType:"Egr, Prof",
+        status:"Active"
+
+    },
+    
+    {
+        id: 4,
+        name:"Raza Muhammad ",
+        img :"/noman.jpg",
+        email:"rmuhammad@uit.edu",
+        UserType:"Dr Prof",
+        status:"Active"
+
+    },
+    
+    {
+        id: 5,
+        name:"Sheraz Ahmed",
+        img :"/noman.jpg",
+        email:"noman@uit.edu",
+        UserType:"Student",
+        status:"In Active"
+
+    },
+    
+    {
+        id: 6,
+        name:"Ayesha Usman",
+        img :"/sample.jpg",
+        email:"rbilal@uit.edu",
+        UserType:"Egr, Prof",
+        status:"Active"
+
+    },
+    
+]

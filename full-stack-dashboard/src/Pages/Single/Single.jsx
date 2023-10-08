242750194c0606fee08d9f1e6057026773247057
Noman <noman5456318@gmail.com>
2023-10-08 16:12:46 +0500

    ui of single page

--- full-stack-dashboard/src/Pages/Single/Single.jsx
+++ full-stack-dashboard/src/Pages/Single/Single.jsx
@@ -10,7 +10,23 @@ const Single = () => {
       <Sidebar/>
       <div className="singleContainer">
         <Navbar/>
-        Single
+        <div className="top">
+          <div className="left">
+            <div className="editBtn">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img className='itemImg' src="https://images.pexels.com/photos/4145069/pexels-photo-4145069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img" />
+
+              detail
+            </div>
+          </div>
+          <div className="right"></div>
+
+        </div>
+
+        <div className="bottom">
+
+        </div>
         
     
     </div>

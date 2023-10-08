f4c00429e4669253c078082fa346146b79ed03d5
Noman <noman5456318@gmail.com>
2023-10-08 15:09:59 +0500

    single page started

--- full-stack-dashboard/src/Pages/Single/Single.jsx
+++ full-stack-dashboard/src/Pages/Single/Single.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Navbar from '../../Components/Navbar/Navbar'
+import './Single.scss'
 
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='Single'>
+      <Sidebar/>
+      <div className="singleContainer">
+        <Navbar/>
+        Single
+        
+    
+    </div>
+    </div>
   )
 }
 

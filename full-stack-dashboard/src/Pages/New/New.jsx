64c352260401798591cb73c5ac3eb58a8040074b
Noman <noman5456318@gmail.com>
2023-10-08 19:43:38 +0500

    new page started

--- full-stack-dashboard/src/Pages/New/New.jsx
+++ full-stack-dashboard/src/Pages/New/New.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './New.scss'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Navbar from '../../Components/Navbar/Navbar'
 
 const New = () => {
   return (
     <div className="New">
         <Sidebar/>
+        <div className="newContainer">
+          <Navbar/>
+          New
+        </div>
 
     </div>
   )

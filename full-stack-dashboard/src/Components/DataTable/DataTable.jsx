5d6b2be1befb7726d5d136184cc140ec7a27051a
Noman <noman5456318@gmail.com>
2023-10-08 11:29:38 +0500

    table completed

--- full-stack-dashboard/src/Components/DataTable/DataTable.jsx
+++ full-stack-dashboard/src/Components/DataTable/DataTable.jsx
@@ -8,11 +8,30 @@ import { userColums, userData } from '../../DataTableSource';
 
 
 const DataTable = () => {
+
+  // Actions
+
+      const actionColumn=[
+        {
+          field: "action",
+          headerName:"Action",
+          width:200,
+          renderCell:()=>{
+            return(
+              <div className="cellAction">
+        <button className="viewButton">View</button>
+        <button className="deleteButton">Delete</button>
+      </div>
+            )
+          }
+        }
+      ]
+
   return (
     <div className='dataTable' style={{ height: 400, width: '100%' }}>
     <DataGrid
       rows={userData}
-      columns={userColums}
+      columns={userColums.concat(actionColumn)}
       initialState={{
         pagination: {
           paginationModel: { page: 0, pageSize: 5 },

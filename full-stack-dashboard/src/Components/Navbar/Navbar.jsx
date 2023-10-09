48ab664389277d4d7565f4db5ccb7c17c82fa0a8
Noman <noman5456318@gmail.com>
2023-10-10 01:52:20 +0500

    project completed

--- full-stack-dashboard/src/Components/Navbar/Navbar.jsx
+++ full-stack-dashboard/src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,66 @@
-import React from 'react'
-import SavedSearchIcon from '@mui/icons-material/SavedSearch';
-import LanguageIcon from '@mui/icons-material/Language';
-import './Navbar.scss'
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import FullscreenIcon from '@mui/icons-material/Fullscreen';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
-import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-
+import React, { useContext } from "react";
+import SavedSearchIcon from "@mui/icons-material/SavedSearch";
+import LanguageIcon from "@mui/icons-material/Language";
+import "./Navbar.scss";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import { DarkModeContext } from "../../Context/darkModeContext";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 const Navbar = () => {
+  const { dispatch, darkMode } = useContext(DarkModeContext);
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" placeholder='Search...' />
-          <SavedSearchIcon/>
-          
+          <input type="text" placeholder="Search..." />
+          <SavedSearchIcon />
         </div>
         <div className="items">
           <div className="item">
-                <LanguageIcon  className='icon'  />
-                English
+            <LanguageIcon className="icon" />
+            English
           </div>
           <div className="item">
-                <Brightness4Icon className='icon' />
+            {darkMode ? (
+              <LightModeIcon
+                onClick={() => {
+                  dispatch({ type: "TOGGLE" });
+                }}
+              />
+            ) : (
+              <DarkModeIcon
+                onClick={() => {
+                  dispatch({ type: "TOGGLE" });
+                }}
+              />
+            )}
           </div>
           <div className="item">
-                <FullscreenIcon className='icon' />
+            <FullscreenIcon className="icon" />
           </div>
           <div className="item">
-                <CircleNotificationsIcon className='icon' />
-                {/* <div className="counter">1</div> */}
+            <CircleNotificationsIcon className="icon" />
+            {/* <div className="counter">1</div> */}
           </div>
           <div className="item">
-               <ChatBubbleIcon className='icon' />
-                {/* <div className="counter">1</div> */}
+            <ChatBubbleIcon className="icon" />
+            {/* <div className="counter">1</div> */}
           </div>
           <div className="item">
-               <FormatListBulletedIcon className='icon' />
+            <FormatListBulletedIcon className="icon" />
           </div>
           <div className="item">
-              <img src="/avatar.jpg" alt="avatar" className='avatar'/>
+            <img src="/avatar.jpg" alt="avatar" className="avatar" />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
